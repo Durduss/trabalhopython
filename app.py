719574af307e4577155358c1bbcb64d13d3ab4d1
guilhermerodriguesti <57836353+guilhermerodriguesti@users.noreply.github.com>
2022-04-16 19:54:29 -0300

    Update and rename exer2 to app.py
    Dudu a questão pode ser abordada de varias formas: com if, while,for e por ai vai. A sua maior confusão estava na atribuição de variáveis que em determinados momentos voce poderia confundir e levando voce a pensar que era um problema de logica.Nesse commit estou usando funções da propria linguagem que deixa o código ate mais limpo(Clean Code) e facil de ler.Claro, eh so um codigo exemplo.Existe outras maneiras de fazer.Então procure sempre utilizar nomes que sao intuitivos facilita muito o debug.Consulte: https://peps.python.org/pep-0008/#function-and-variable-names

---
+++ app.py
@@ -0,0 +1,7 @@
+numeros_lista = []
+
+for numeros in range(5): 
+    numeros_lista.append(int(input('Digite um valor inteiro: ')))
+
+print ('O maior valor é', max(numeros_lista))
+print ('O menor valor é', min(numeros_lista))  
